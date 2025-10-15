d7fbd74654318dae7fac2e126bd208c0f34df0ab
timhealey3 <timhealey34@gmail.com>
2025-10-15 14:13:40 -0400

    re-write main logic

--- src/client/main.py
+++ src/client/main.py
@@ -1,24 +1,36 @@
 import requests
 import json
 
+GENERIC_ERROR_OCCURRED = "An error has occured"
+COULD_NOT_CONNECT_MCP_SERVER = "Could not connect to MCP server"
+
 def ask(prompt):
     try:
         response = requests.post(
             "http://localhost:8080",
             json={"prompt": prompt}
         )
-        print("Gemma3:4b:", response.json()["response"])
+        displayResponse(response)
     except:
-        raise ConnectionRefusedError("Could not connect to MCP server")
+        raise ConnectionRefusedError(COULD_NOT_CONNECT_MCP_SERVER)
 
-if __name__ == "__main__":
+def displayResponse(response):
+    print("Gemma3:4b:", response.json()["response"])
+
+
+def coreLoop():
     while True:
         user_input = input("> ")
         if user_input.lower() in ["q", "quit"]:
-            break
+            return False
         try:
             ask(user_input)
         except ConnectionRefusedError:
-            print("Could not connect to MCP server")
+            print(COULD_NOT_CONNECT_MCP_SERVER)
         except:
-            print("An error has occured")
+            print(COULD_NOT_CONNECT_MCP_SERVER)
+
+
+if __name__ == "__main__":
+    coreLoop()
+        
